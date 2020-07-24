49ef1ca2a53ab024f5f688900472f4e7f5edfbc8
eagletsai <eagletsai@google.com>
2020-07-24 22:55:08 +0800

    Initial commit

---
+++ src/main/java/com/ego/Hello.java
@@ -0,0 +1,9 @@
+package com.ego;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello");
+        Person ego = new Person();
+        ego.hello();
+    }
+}
---
+++ src/main/kotlin/com/ego/Hello.kt
@@ -0,0 +1,13 @@
+package com.ego
+
+fun main() {
+    println("hello")
+    val h = Human()
+    h.hello()
+}
+
+class Human(){
+    fun hello(){
+        println("hello")
+    }
+}
